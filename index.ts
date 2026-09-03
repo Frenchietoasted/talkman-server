b9d7eea67246d438f822fff41b930ad03cf1aac3
Steve Dmello <steveaarond@gmail.com>
2026-09-03 15:55:47 +0530

    feat: something

--- index.ts
+++ index.ts
@@ -2,16 +2,23 @@ import express from "express";
 import { WebSocketServer } from "ws";
 
 const app = express();
+let users: { name: string, url: string }[] = [];
+const wss = new WebSocketServer({ port: 8080, clientTracking: true });
 
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", function connection(ws) {
-    ws.on("message", function message(data) {
-        console.log("received:", data.toString());
-    });
+wss.once("connection", function connection(ws) {
+  console.log("welcome to the chat: what's your name?\n ");
+  console.log(wss.clients);
+  ws.on("message", function message(data) {
+    const name = data.toString();
+    users.push({ name, url: ws.url });
+    console.log("received:", name);
+    ws.send(`Hello ${name}!`);
+  });
 
     ws.send("Hello from server!");
 });
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
