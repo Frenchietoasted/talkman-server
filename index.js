c8ef0e7b30cc9b510cb9684606e09765e19f4940
Steve Dmello <steveaarond@gmail.com>
2026-09-03 22:54:09 +0530

    fix: build fixes

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var ws_1 = require("ws");
-var app = (0, express_1.default)();
-var wss = new ws_1.WebSocketServer({ port: 8080 });
+//bun --hot run index.ts to run the server with hot reloading
+import express from "express";
+import { WebSocketServer } from "ws";
+const app = express();
+let users = [];
+const wss = new WebSocketServer({ port: 8080, clientTracking: true });
 wss.on("connection", function connection(ws) {
+    ws.send("welcome to the chat, what's your name? ");
     ws.on("message", function message(data) {
-        console.log("received:", data.toString());
+        const name = data.toString();
+        console.log("received:", name);
+        ws.send(`Hello ${name}!`);
+        wss.clients.forEach((client) => {
+            client.send(`User ${name} is in the chat, Say hi!`);
+        });
     });
-    ws.send("Hello from server!");
 });
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
