8f3ba490ecedbd746228027a08f899ec35948156
Steve Dmello <steveaarond@gmail.com>
2026-09-03 22:09:54 +0530

    test: to test render

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+//bun --hot run index.ts to run the server with hot reloading
 import express from "express";
 import { WebSocketServer } from "ws";
 
@@ -5,20 +6,17 @@ const app = express();
 let users: { name: string, url: string }[] = [];
 const wss = new WebSocketServer({ port: 8080, clientTracking: true });
 
-wss.once("connection", function connection(ws) {
-  console.log("welcome to the chat: what's your name?\n ");
-  console.log(wss.clients);
+wss.on("connection", function connection(ws) {
+  ws.send("welcome to the chat, what's your name? ");
   ws.on("message", function message(data) {
     const name = data.toString();
-    users.push({ name, url: ws.url });
     console.log("received:", name);
     ws.send(`Hello ${name}!`);
+    wss.clients.forEach((client) => {
+      client.send(`User ${name} is in the chat, Say hi!`)
+    })
   });
-
-    ws.send("Hello from server!");
 });
-
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
